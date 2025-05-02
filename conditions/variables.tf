e077151e0651744b5d1c447c2a1ce131afa2e549
sivakumar <sivapavan490@gmail.com>
2025-05-02 12:53:05 +0530

    variables

---
+++ conditions/variables.tf
@@ -0,0 +1,44 @@
+variable "ami_id"{
+    type = string
+    default = "ami-09c813fb71547fc4f"
+}
+
+variable "environment" {
+        default = "dev"
+}   
+variable "instance_type" {
+        default = "t2.micro"
+}   
+
+variable "tags" {
+    type = map
+    default = {
+        Name= "backend"
+        project="expense"
+        environment="dev"
+        
+    }
+}
+
+variable "from_port" {
+    type = number
+    default = 22
+}
+
+variable "to_port" {
+  type = number
+  default = 22
+}
+
+variable "cidr_blocks" {
+  type = list
+  default = ["0.0.0.0/0"]
+}
+
+variable "sg_tags" { 
+  type = map
+  default = {
+    Name = "expense-backend-dev"
+  }
+
+}
