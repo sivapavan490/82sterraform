8515a22973b2f66076647e781f8410ac47951f7e
sivakumar <sivapavan490@gmail.com>
2025-05-02 12:16:12 +0530

    variables

---
+++ variables/ec2.tf
@@ -0,0 +1,30 @@
+
+resource "aws_instance" "backend" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.securitygroup_creation.id] # refering security group resource into instance resource
+
+  tags = var.tags
+}
+
+
+resource "aws_security_group" "securitygroup_creation" {
+  name        = "backend" # security group name
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  ingress {
+    from_port   = var.from_port
+    to_port     = var.to_port
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  tags = var.sg_tags
+}
+
